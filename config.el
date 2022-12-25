151e46ef41d782363ae79ef6e20077fe367b8edb
hegde-atri <iamatrihegde@outlook.com>
2022-12-25 15:14:36 +0000

    init

---
+++ config.el
@@ -0,0 +1,5 @@
+(require 'package)
+(add-to-list 'package-archives
+             '("melpa" . "https://melpa.org/packages/"))
+(package-refresh-contents)
+(package-initialize)
---
+++ init.el
@@ -0,0 +1,3 @@
+(org-babel-load-file
+ (expand-file-name
+  "~/repos/linux/.emacs.d/config.org"))
